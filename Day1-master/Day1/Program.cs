2d4ef52c2da6074f09df32863464f075a1259b0d
zhaoyinqing <e0283993@u.nus.edu>
2018-07-07 10:09:03 +0800

    Add project files.

---
+++ Day1-master/Day1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //writeline means writing the string and then change line
+            Console.WriteLine("Hello world");
+            Console.WriteLine("****\n*******");
+            Console.WriteLine("*         *");
+            Console.WriteLine("***********");
+
+            //write means writing the string to the screen
+            Console.Write("Hello");
+            Console.Write("ISS");
+
+            Console.WriteLine("Hello");
+            Console.WriteLine("ISS");
+
+            Console.WriteLine("No\tItem\tPrice");
+            Console.WriteLine("1\tShoe\t$20");
+            Console.WriteLine("2\tShirt\t$25");
+
+            Console.WriteLine("She said: \"Hi\".");
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.WriteLine("***********");
+            Console.WriteLine("*         *");
+            Console.WriteLine("***********");
+        }
+    }
+
+}
